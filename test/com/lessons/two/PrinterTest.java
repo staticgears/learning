4e5ec71e8f63d59664ca611552dd69d81ceb2617
Keith Banks <staticgears@gmail.com>
2015-09-06 19:12:02 +0200

    Adding fibonacci example

--- test/com/lessons/two/PrinterTest.java
+++ test/com/lessons/two/PrinterTest.java
@@ -3,6 +3,9 @@ package com.lessons.two;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Date: 06/09/15
  *
@@ -10,9 +13,45 @@ import org.junit.Test;
  */
 public class PrinterTest extends TestCase {
 
+
     @Test
-    public void testAddStuff() throws Exception {
+    public void testAddStuff() {
         Printer print = new Printer();
         print.addStuff();
     }
+
+    @Test
+    public void testF() {
+        int size = 100;
+        for(int n=0; n <= size; n++) {
+            System.out.println("result for n: " + n + " is : " + f(n));
+        }
+    }
+
+    private long f(int x) {
+        return f(x, 1,  1);
+    }
+
+    private long f(int x, long a, long b) {
+        if (x <= 1) return a;
+        long t = a;
+        a += b;
+        b = t;
+        return f(x-1, a, b);
+    }
+
+    @Test
+    public void testF2() {
+        int size = 100;
+        for(int n=0; n <= size; n++) {
+            System.out.println("result for n: " + n + " is : " + f2(n));
+        }
+    }
+
+    private long f2(int x) {
+        if(x < 2) return x;
+        if(x == 44) return 701408733;
+        return  f(x-1) + f(x-2);
+    }
+
 }

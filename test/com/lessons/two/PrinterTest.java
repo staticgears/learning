bc1df886a1d4c695679777975ece9c6c8325905a
Keith Banks <staticgears@gmail.com>
2015-09-07 08:27:23 +0200

    Added the map version of fibonacci back to the tests for you as an example

--- test/com/lessons/two/PrinterTest.java
+++ test/com/lessons/two/PrinterTest.java
@@ -28,6 +28,22 @@ public class PrinterTest extends TestCase {
         }
     }
 
+    @Test
+    public void testF2() {
+        int size = 100;
+        for(int n=0; n <= size; n++) {
+            System.out.println("result for n: " + n + " is : " + f2(n));
+        }
+    }
+
+    @Test
+    public void testF3() {
+        int size = 100;
+        for(int n=0; n <= size; n++) {
+            System.out.println("result for n: " + n + " is : " + f3(n));
+        }
+    }
+
     private long f(int x) {
         return f(x, 1,  1);
     }
@@ -40,18 +56,20 @@ public class PrinterTest extends TestCase {
         return f(x-1, a, b);
     }
 
-    @Test
-    public void testF2() {
-        int size = 100;
-        for(int n=0; n <= size; n++) {
-            System.out.println("result for n: " + n + " is : " + f2(n));
-        }
-    }
 
     private long f2(int x) {
         if(x < 2) return x;
         if(x == 44) return 701408733;
         return  f(x-1) + f(x-2);
+    }
+
+    Map<Integer, Long> cache = new HashMap<>();
+    private long f3(int x) {
+        if(x < 2) return x;
+        if(cache.containsKey(x)) return cache.get(x);
+        long result = f(x-1) + f(x-2);
+        cache.put(x, result);
+        return result;
     }
 
 }
